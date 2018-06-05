09bafd24d4dca970fe77787eedceba9ce0837e62
septian97 <septian.rizaldi@yahoo.com>
2018-06-05 11:33:44 +0700

    upload uas

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php
     class Koneksi{
         private $server = "localhost";
-        private $username = "root";
-        private $password = "";
-        private $db = "mahasiswa";
+        private $username = "id4993070_septian";
+        private $password = "septian123";
+        private $db = "id4993070_data_mahasiswa";
 
         function getKoneksi(){
             return new mysqli($this->server, $this->username, $this->password, $this->db);
